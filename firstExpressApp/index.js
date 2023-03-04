49c71ec7af32e8dc242d1b7eafbb141479f590c4
Michael Valdez <mikeyvaldez92@yahoo.com>
2023-03-04 06:50:58 -0800

    added express routes

--- firstExpressApp/index.js
+++ firstExpressApp/index.js
@@ -1,8 +1,31 @@
 const express = require('express')
 const app = express()
 
-app.use(() => {
-  console.log('WE GOT A NEW REQUEST!!')
+// app.use(() => {
+//   console.log('WE GOT A NEW REQUEST!!')
+// })
+
+app.get('/', (req, res) => {
+  res.send('This is the home page')
+})
+
+app.post('/cats', (req, res) => {
+  res.send('POST REQUEST TO /cats!!!!!! THIS IS NOT A GET REQUEST!')
+})
+
+app.get('/cats', (req, res) => {
+  res.send('MEOW!!!')
+})
+
+app.get('/dogs', (req, res) => {
+  res.send('WOOF!!!')
+})
+
+// must be on the bottom because it refers to unspecified routes
+// if placed at the top every path is unknown becuase this get
+// request will render first
+app.get('*', (req, res) => {
+  res.send(`I dont't know that path!!!`)
 })
 
 app.listen(3000, () => {
